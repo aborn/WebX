a9f6bf4f50fdf63788c37ffdce4ffda36b664a55
GitHub Action <action@github.com>
2021-01-29 05:57:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/aborn/webdevtoolkit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aborn/webdevtoolkit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aborn.webdevtoolkit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aborn.webdevtoolkit.MyBundle
 
 class MyApplicationService {
 
