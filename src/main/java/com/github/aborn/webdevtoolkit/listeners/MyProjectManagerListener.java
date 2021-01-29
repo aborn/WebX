d73c3a61165dbadde437e1841053ff46448fbbbe
aborn <loveaborn@foxmail.com>
2021-01-29 15:58:08 +0800

    init

---
+++ src/main/java/com/github/aborn/webdevtoolkit/listeners/MyProjectManagerListener.java
@@ -0,0 +1,37 @@
+package com.github.aborn.webdevtoolkit.listeners;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author aborn
+ * @date 2021/01/29 2:56 PM
+ */
+public class MyProjectManagerListener implements ProjectManagerListener {
+    @Override
+    public void projectOpened(@NotNull Project project) {
+        // Ensure this isn't part of testing
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            return;
+        }
+        Messages.showMessageDialog(project, "打开！", project.getName(), Messages.getInformationIcon());
+    }
+
+    /**
+     * Invoked on project close.
+     *
+     * @param project closing project
+     */
+    @Override
+    public void projectClosed(@NotNull Project project) {
+        // Ensure this isn't part of testing
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            return;
+        }
+        Messages.showMessageDialog(project, "关闭！", project.getName(), Messages.getInformationIcon());
+    }
+
+}
---
+++ src/main/java/com/github/aborn/webdevtoolkit/services/MyApplicationService.java
@@ -0,0 +1,8 @@
+package com.github.aborn.webdevtoolkit.services;
+
+/**
+ * @author aborn
+ * @date 2021/01/29 2:45 PM
+ */
+public class MyApplicationService {
+}
