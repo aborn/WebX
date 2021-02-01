f6878d5abeb2e22cd8e02c6d9fea9314a993ddcc
aborn <loveaborn@foxmail.com>
2021-02-01 21:28:48 +0800

    goto services actions

---
+++ src/main/java/com/github/aborn/webdevtoolkit/actions/GotoServicesAction.java
@@ -0,0 +1,17 @@
+package com.github.aborn.webdevtoolkit.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author aborn
+ * @date 2021/02/01 9:27 PM
+ */
+public class GotoServicesAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
