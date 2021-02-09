dee841b7a5f052e12822b1e8413a976ccc3d84fe
aborn <loveaborn@foxmail.com>
2021-02-09 11:56:04 +0800

    add TimeTrace module

---
+++ src/main/java/com/github/aborn/webx/modules/tc/ActionPoint.java
@@ -0,0 +1,16 @@
+package com.github.aborn.webx.modules.tc;
+
+import java.math.BigDecimal;
+
+/**
+ * 用户交互点
+ * @author aborn
+ * @date 2021/02/09 10:43 AM
+ */
+public class ActionPoint {
+    String entity;
+    BigDecimal timestamp;
+    Boolean isWrite;
+    String project;
+    String language;
+}
---
+++ src/main/java/com/github/aborn/webx/modules/tc/TimeTrace.java
@@ -0,0 +1,59 @@
+package com.github.aborn.webx.modules.tc;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+/**
+ * @author aborn
+ * @date 2021/02/09 10:46 AM
+ */
+public class TimeTrace {
+
+    public static Boolean READY = false;
+    // 定时上报任务
+    private static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private static ScheduledFuture<?> scheduledFixture;
+
+    private final int queueTimeoutSeconds = 30;
+    private static ConcurrentLinkedQueue<ActionPoint> heartbeatsQueue = new ConcurrentLinkedQueue<ActionPoint>();
+
+    private void setupQueueProcessor() {
+        final Runnable handler = new Runnable() {
+            public void run() {
+                processHeartbeatQueue();
+            }
+        };
+        long delay = queueTimeoutSeconds;
+        scheduledFixture = scheduler.scheduleAtFixedRate(handler, delay, delay, java.util.concurrent.TimeUnit.SECONDS);
+    }
+
+    private static void processHeartbeatQueue() {
+        if (TimeTrace.READY) {
+
+            // get single heartbeat from queue
+            ActionPoint heartbeat = heartbeatsQueue.poll();
+            if (heartbeat == null) {
+                return;
+            }
+
+            // get all extra heartbeats from queue
+            ArrayList<ActionPoint> actionPoints = new ArrayList<ActionPoint>();
+            actionPoints.add(heartbeat);
+            while (true) {
+                ActionPoint h = heartbeatsQueue.poll();
+                if (h == null)
+                    break;
+                actionPoints.add(h);
+            }
+
+            sendHeartbeat(actionPoints);
+        }
+    }
+
+    private static void sendHeartbeat(final ArrayList<ActionPoint> extraHeartbeats) {
+        // 处理发送消息
+    }
+}
