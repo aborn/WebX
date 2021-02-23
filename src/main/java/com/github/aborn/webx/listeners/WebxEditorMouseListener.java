cb2f35fe4a10ade58fd65f4b1125c29bba380928
aborn <loveaborn@foxmail.com>
2021-02-23 15:43:25 +0800

    修复mouseclick事件监听不到bug，并控制好频率

--- src/main/java/com/github/aborn/webx/listeners/WebxEditorMouseListener.java
+++ src/main/java/com/github/aborn/webx/listeners/WebxEditorMouseListener.java
@@ -6,6 +6,8 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
+import java.util.Date;
+
 /**
  * @author aborn
  * @date 2021/02/08 4:09 PM
@@ -13,10 +15,14 @@ import com.intellij.openapi.vfs.VirtualFile;
 public class WebxEditorMouseListener extends UserActionBaseListener implements EditorMouseListener {
     public WebxEditorMouseListener() {}
 
+    private static Date lastRecord = null;
+
     @Override
     public void mousePressed(EditorMouseEvent editorMouseEvent) {
-        info("mouse pressed.");
-        record();
+        if (isNeedRecord()) {
+            info("mouse pressed.");
+            record();
+        }
 
         /** 先不用处理
         FileDocumentManager instance = FileDocumentManager.getInstance();
@@ -28,8 +34,27 @@ public class WebxEditorMouseListener extends UserActionBaseListener implements E
 
     @Override
     public void mouseEntered(EditorMouseEvent editorMouseEvent) {
-        info("mouse entered.");
-        record();
+        if (isNeedRecord()) {
+            record();
+        }
     }
 
+    /**
+     * 每固定时间（5s）记录一次，免得事件太过频繁
+     * @return
+     */
+    private boolean isNeedRecord() {
+        if (lastRecord == null) {
+            lastRecord = new Date();
+            return true;
+        } else {
+            Date currentDate = new Date();
+            if ((currentDate.getTime() - lastRecord.getTime()) / 1000 > 5) {
+                lastRecord = new Date();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
