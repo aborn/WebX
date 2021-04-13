ad879e535657a061946692b0d59860d5275a864a
aborn <loveaborn@foxmail.com>
2021-04-13 13:52:16 +0800

    webx.ts

---
+++ webx-vscode/src/webx.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+import * as events from "./events";
+
+export class WebX {
+    constructor(state: vscode.Memento) {
+        this.initEventListeners()
+    }
+
+    private initEventListeners(): void {
+        let events: vscode.Disposable[] = [];
+        vscode.workspace.onDidChangeTextDocument(this.onEdit, this, events);
+    }
+
+    private onEdit(e: vscode.TextDocumentChangeEvent) {
+        let eventName = events.FILE_EDITED;
+        if (e.contentChanges.length > 0) {
+            this.record();
+        }
+    }
+
+    private record() {
+        console.log('record.')
+    }
+
+    public dispose() {
+        // clear all temp state and post current data
+    }
+}
