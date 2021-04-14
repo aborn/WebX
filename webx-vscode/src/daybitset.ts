8484e4419fd20069cb94037070c72f0ec78323b8
aborn <loveaborn@foxmail.com>
2021-04-15 07:06:24 +0800

    bit dealing

--- webx-vscode/src/daybitset.ts
+++ webx-vscode/src/daybitset.ts
@@ -5,16 +5,34 @@ const SLOT_SIZE = 24 * 60 * 2;
 export class DayBitSet {
     private day: string;
     private bitset: BitSet;
-    
 
     constructor() {
         this.day = "2021-04-07";
         this.bitset = new BitSet(SLOT_SIZE);
+        this.bitset.clear();
         // this.bitset.setRange(0, SLOT_SIZE);
     }
 
     public record(): void {
-        this.bitset.set(dateutils.getSlotIndex(), 1);
+        let slot = dateutils.getSlotIndex();
+        this.bitset.set(slot, 1);
     }
 
+    public cardinality(): number {
+        return this.bitset.cardinality();
+    }
+
+    public print(): void {
+        let index = 0;
+        for (let b of this.bitset) {
+            if (b === 1) {
+                console.log(index + ':' + b + '\n');
+            }
+            index = index + 1;
+        }
+        console.log('last index=' + index);
+        let arr = this.bitset.toArray();
+        console.log("array length = " + arr.length);
+        console.log(arr);
+    }
 }
--- webx-vscode/src/main.ts
+++ webx-vscode/src/main.ts
@@ -1,4 +1,10 @@
 import * as dateutils from "./dateutils";
+import { DayBitSet } from "./daybitset";
 
 console.log('start runing');
 console.log('slotIndex=' + dateutils.getSlotIndex());
+
+let daybitset = new DayBitSet();
+daybitset.record();
+console.log('cardinality = ' + daybitset.cardinality());
+daybitset.print();
